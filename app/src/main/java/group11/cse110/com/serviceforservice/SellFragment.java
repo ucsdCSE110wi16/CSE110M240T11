bf5f17dd1af1a97fd84412f29e122c8df89480ba
Jennifer Phung <jenniferphung11@gmail.com>
2016-01-26 19:45:28 -0800

    Added SellPageTwo, added some code into the sell java pages

--- app/src/main/java/group11/cse110/com/serviceforservice/SellFragment.java
+++ app/src/main/java/group11/cse110/com/serviceforservice/SellFragment.java
@@ -1,20 +1,48 @@
 package group11.cse110.com.serviceforservice;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 /**
  * Created by Kim on 1/18/16.
  */
 public class SellFragment extends Fragment {
 
+    //Keeps track of which radio button was selected
+    static int sellCategory = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.sell, container, false);
+
+
+        Button next= (Button)rootView.findViewById(R.id.submitButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new sellPageTwo();
+
+                Bundle bundle = new Bundle();
+                //Not really sure what to do here, since we'd be getting our info from
+                //onActvityCreated right? Also, would I be putting twice?? Once for
+                //what to sell, and once for what they are willing to exchange for?
+                bundle.putInt(sell, sellCategory);
+                fragment.setArguments(bundle);
+
+                android.support.v4.app.FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.sell, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+
+            }
+        });
 
         return rootView;
     }
---
+++ app/src/main/java/group11/cse110/com/serviceforservice/sellPageTwo.java
@@ -0,0 +1,27 @@
+package group11.cse110.com.serviceforservice;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Phung on 1/18/16.
+ */
+public class sellPageTwo extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.buy, container, false);
+
+        Bundle bundle = this.getArguments();
+        int defaultValue = 0;
+        int sellSelection = bundle.getInt(sell, defaultValue);
+
+        return rootView;
+    }
+
+}
