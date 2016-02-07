b1e33036fbd9f2b4c6098cb4a8e16fa668bb6c6d
dasitu5 <dasitu@ucsd.edu>
2016-02-06 16:37:50 -0800

    Added Facebook Login

--- app/src/main/java/group11/cse110/com/serviceforservice/StartScreen.java
+++ app/src/main/java/group11/cse110/com/serviceforservice/StartScreen.java
@@ -1,24 +1,31 @@
 package group11.cse110.com.serviceforservice;
 
-import android.app.Activity;
-
 import android.annotation.TargetApi;
-import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
+
+import com.facebook.login.widget.LoginButton;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class StartScreen extends Activity implements OnClickListener {
     Intent facebook;
     Intent signupIntent;
     ImageButton sign;
-    ImageButton fb;
+    LoginButton fb;
     Intent camera;
 
     @Override
@@ -29,9 +36,10 @@ public class StartScreen extends Activity implements OnClickListener {
         signupIntent = new Intent(this, SignIn.class);
         facebook = new Intent(this, SignUp.class);
         sign = (ImageButton) findViewById(R.id.signup);
-        fb = (ImageButton) findViewById(R.id.signfacebook);
+        fb = (LoginButton) findViewById(R.id.signfacebook);
         sign.setOnClickListener(this);
         fb.setOnClickListener(this);
+
     }
 
     @Override
@@ -42,8 +50,8 @@ public class StartScreen extends Activity implements OnClickListener {
                 startActivity(signupIntent);
                 break;
             case (R.id.signfacebook):
-                startActivity(facebook);
-                break;
+                //startActivity(facebook);
+                //break;
         }
     }
 
